8ef4f6502c2dcef1798b4d2b682d6856d9281c2b
hrxiang <xianghairui@126.com>
2022-01-06 12:50:36 +0800

    新增9宫格头像

--- lib/src/chat_avatar_view.dart
+++ lib/src/chat_avatar_view.dart
@@ -19,11 +19,13 @@ class ChatAvatarView extends StatelessWidget {
     this.onTap,
     this.url,
     this.onLongPress,
-    this.isCircle = true,
+    this.isCircle = false,
     this.borderRadius,
     this.text,
     this.textStyle,
     this.lowMemory = false,
+    this.isNineGrid = false,
+    this.nineGridUrls = const [],
   }) : super(key: key);
   final bool visible;
   final double? size;
@@ -35,6 +37,8 @@ class ChatAvatarView extends StatelessWidget {
   final String? text;
   final TextStyle? textStyle;
   final bool lowMemory;
+  final List<String> nineGridUrls;
+  final bool isNineGrid;
 
   double get _size => size ?? 42.h;
 
@@ -42,19 +46,24 @@ class ChatAvatarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var child = isNineGrid ? _nineGridAvatar() : _normalAvatar();
     return Visibility(
       visible: visible,
-      child: /* isCircle
-          ? ClipOval(
-              child: _childAvatar(),
-            )
-          : */
-          ClipRRect(
-        child: _childAvatar(),
-        borderRadius: borderRadius ?? BorderRadius.circular(6),
-      ),
+      child: isCircle
+          ? ClipOval(child: child)
+          : ClipRRect(
+              child: child,
+              borderRadius: borderRadius ?? BorderRadius.circular(6),
+            ),
     );
   }
+
+  Widget _normalAvatar() => /*isCircle*/ false
+      ? ClipOval(child: _childAvatar())
+      : ClipRRect(
+          child: _childAvatar(),
+          borderRadius: borderRadius ?? BorderRadius.circular(6),
+        );
 
   Widget _childAvatar() => Container(
         width: _size,
@@ -117,4 +126,135 @@ class ChatAvatarView extends StatelessWidget {
           loadProgress: false,
           cacheWidth: (1.sw * .25).toInt(),
         );
+
+  Widget _nineGridImage(String url, double size) => IconUtil.networkImage(
+        url: url,
+        width: size,
+        height: size,
+        fit: BoxFit.cover,
+        loadProgress: false,
+        cacheWidth: size.toInt(),
+        cacheHeight: size.toInt(),
+      );
+
+  Widget _nineGridAvatar() => Container(
+        width: _size,
+        height: _size,
+        color: Colors.grey[300],
+        padding: EdgeInsets.all(2.0),
+        alignment: Alignment.center,
+        child: _nineGridColumn(),
+      );
+
+  /// 9宫格列
+  Widget _nineGridColumn() {
+    var width;
+    var margin = 2.0;
+    var row1Length;
+    var row2Length;
+    var row3Length;
+    var list = <Widget>[];
+    switch (nineGridUrls.length) {
+      case 1:
+        width = _size;
+        row1Length = 1;
+        break;
+      case 2:
+        width = _size / 2;
+        row1Length = 2;
+        break;
+      case 3:
+        width = _size / 2;
+        row1Length = 1;
+        row2Length = 2;
+        break;
+      case 4:
+        width = _size / 2;
+        row1Length = 2;
+        row2Length = 2;
+        break;
+      case 5:
+        width = _size / 3;
+        row1Length = 2;
+        row2Length = 3;
+        break;
+      case 6:
+        width = _size / 3;
+        row1Length = 3;
+        row2Length = 3;
+        break;
+      case 7:
+        width = _size / 3;
+        row1Length = 1;
+        row2Length = 3;
+        row3Length = 3;
+        break;
+      case 8:
+        width = _size / 3;
+        row1Length = 2;
+        row2Length = 3;
+        row3Length = 3;
+        break;
+      case 9:
+        width = _size / 3;
+        row1Length = 3;
+        row2Length = 3;
+        row3Length = 3;
+        break;
+    }
+    if (row1Length > 0) {
+      list.add(_nineGridRow(
+        length: row1Length,
+        start: 0,
+        size: width,
+        margin: margin,
+      ));
+    }
+    if (row2Length > 0) {
+      list.add(_nineGridRow(
+        length: row2Length,
+        start: row1Length,
+        size: width,
+        margin: margin,
+      ));
+    }
+    if (row3Length > 0) {
+      list.add(_nineGridRow(
+        length: row3Length,
+        start: row1Length + row2Length,
+        size: width,
+        margin: margin,
+      ));
+    }
+    return Column(
+      children: list,
+    );
+  }
+
+  /// 9宫格行
+  Widget _nineGridRow({
+    required int length,
+    required int start,
+    required double size,
+    required double margin,
+  }) {
+    var list = <Widget>[];
+    for (var i = 0; i < length; i++) {
+      start += i;
+      list.add(_nineGridImage(nineGridUrls.elementAt(start), size));
+      if (i != length - 1) {
+        list.add(_nineGridLine(width: margin, height: size));
+      }
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: list,
+    );
+  }
+
+  Widget _nineGridLine({
+    double? width,
+    double? height,
+  }) =>
+      Container(height: height, width: width, color: Colors.white);
 }
