bd5dd57bcd45197f5efa72fad28aacf7fc7dc6b8
hrxiang <xianghairui@126.com>
2022-07-31 17:29:59 +0800

    Fix bug

--- lib/src/chat_at_text.dart
+++ lib/src/chat_at_text.dart
@@ -138,8 +138,8 @@ class ChatAtText extends StatelessWidget {
           if (mapping.type == PatternType.EMOJI) {
             inlineSpan = ImageSpan(
               ImageUtil.emojiImage(matchText),
-              imageWidth: style.fontSize!,
-              imageHeight: style.fontSize!,
+              imageWidth: style.fontSize! * 1.4,
+              imageHeight: style.fontSize! * 1.4,
             );
           } else {
             inlineSpan = TextSpan(
