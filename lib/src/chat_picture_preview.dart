221bcc25f5576f322acdf82ba977d7fcea170ef0
hrxiang <xianghairui@126.com>
2023-02-09 15:17:42 +0800

    修改文件下载时的接收时间

--- lib/src/chat_picture_preview.dart
+++ lib/src/chat_picture_preview.dart
@@ -255,7 +255,7 @@ class ChatPicturePreview extends StatelessWidget {
     await dio?.download(
       picList.elementAt(index).url!,
       savePath,
-      options: Options(receiveTimeout: 120 * 1000),
+      options: Options(receiveTimeout: 10 * 60 * 1000),
     );
     onDownloadFinished?.call(url, savePath);
   }
